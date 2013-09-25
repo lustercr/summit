44a5451b0eddb91807f7c5adfc34d4e578b1ea75
Norman Murillo <normandanielmq@gmail.com>
2013-09-24 18:02:16 -0600

    Ajustes en el proyecto Agenda

--- agenda/app/js/agenda.js
+++ agenda/app/js/agenda.js
@@ -1,12 +1,10 @@
-//creamos la configuración de nuestro modulo
-var configAgenda = function($routeProvider){
+//creamos el modulo y le aplicamos la configuración
+var Agenda = angular.module("Agenda", []).config(function($routeProvider){
  
     $routeProvider.when("/home", {
         controller: "notasController",
         templateUrl: "views/home.html"
-    });
+    })
+    .otherwise({redirectTo: "/home"});
  
-}
- 
-//creamos el modulo y le aplicamos la configuración
-var Agenda = angular.module("Agenda", []).config(configAgenda);
+});
--- agenda/app/js/models/notasModel.js
+++ agenda/app/js/models/notasModel.js
@@ -5,27 +5,22 @@ Agenda.service("notasModel", function(){
     this.getNotas = function(){
         return [
             {
-                id : 0,
                 nombre : "Israel Salas",
                 edad : "32 años"
             },
             {
-                id : 1,
                 nombre : "Andrés López",
                 edad : "24 años"
             },
             {
-                id : 2,
                 nombre : "Juan Rodríguez",
                 edad : "28 años"
             },
             {
-                id : 3,
                 nombre : "Pepito",
                 edad : "18 años"
             },
             {
-                id : 4,
                 nombre : "Manuel",
                 edad : "45 años"
             }
